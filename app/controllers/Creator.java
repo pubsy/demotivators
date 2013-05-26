45266f08bf9eff1b3a4ddf3b9df157a4b8602b6c
Vitalii Kravets <vitalii.kravets@gmail.com>
2013-05-26 17:14:43 +0300

    Adding preview functionality

--- app/controllers/Creator.java
+++ app/controllers/Creator.java
@@ -2,15 +2,23 @@ package controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+
+import org.apache.commons.lang.StringUtils;
+
+import controllers.Secure.Security;
 
 import models.Demotivator;
 import models.Domain;
 import models.User;
 import play.data.validation.Required;
-import play.data.validation.Validation;
-import play.mvc.Controller;
+import play.i18n.Messages;
+import play.mvc.Util;
 import play.mvc.With;
 import services.DemotivatorCreator;
 
@@ -21,72 +29,83 @@ import services.DemotivatorCreator;
  * @author vitaliikravets
  *
  */
-@With(Secure.class)
+
 public class Creator extends DemotivatorsController{
 	
+	private static final String REQUIRED = "validation.required";
 	private static final String TEXT_CANT_MESSAGE_KEY = "text.cant.be.longer";
 	private static final String TITLE_CANT_MESSAGE_KEY = "title.cant.be.longer";
 	private static final String PLEASE_SELECT_MESSAGE_KEY = "please.select.an.image";
 	private static final String BAD_FILE_MESSAGE_KEY = "bad.file.error";
+	private static final String PLEASE_LOGIN = "please_login";
 	
 	@Inject
 	static DemotivatorCreator creator;
 	
-	public static void add(){		
+	
+	public static void add() throws Throwable{	
+		String userName = Security.connected();		
+		if(userName == null){
+			Secure.login();
+		}
+		
 		render();
 	}
-
-	public static void create(@Required String title, String text, File[] image) throws Throwable {
-
-		validateParameters(title, text, image);
-
-		User user = DemotivatorsSecurity.currentUser();
+	
+	public static void create(@Required String title, String text, File[] image, String mode) throws Throwable {
+		Map<String, String> errors = new HashMap<String, String>();
 		
-		if(user == null){
-			Secure.login();
+		String userName = Security.connected();		
+		if(userName == null){
+			error(403, Messages.get(PLEASE_LOGIN));
+		}
+		
+		validateParameters(errors, title, text, image);
+		if(!errors.keySet().isEmpty()){
+			renderJSON(errors);
 		}
 		
 		String fileName = null;
 		try {
 			fileName = creator.createDemotivator(image[0], title, text);
 		} catch (IOException e) {
-			validation.addError("image", BAD_FILE_MESSAGE_KEY);
-			sendBackWithValidationErrors();
+			errors.put("image_error", Messages.get(BAD_FILE_MESSAGE_KEY));
+			renderJSON(errors);
 		}
 		
-		Domain domain = Domain.getOrCreate(request.domain);
-	
-		Demotivator demo = new Demotivator(title, fileName, user, domain);
-		demo.save();
+		Map<String, String> success = new HashMap<String, String>(3);
 		
-		Application.single(demo.getId());
-	}
-
-	private static void validateParameters(String title, String text, File[] image) {
-		if(title.length() > 30){
-			validation.addError("title", TITLE_CANT_MESSAGE_KEY);
-		}
-		if(text.length() > 80){
-			validation.addError("text", TEXT_CANT_MESSAGE_KEY);
-		}
-		
-		boolean isNullFile = false;
-		if(image == null || image[0] == null){
-			isNullFile = true;
-		}
-		
-		if (Validation.hasErrors() || isNullFile) {
-			//can not populate <input type="file"> with value, so user
-			//has to select a file every time he makes a mistake filling the form.
-			validation.addError("image", PLEASE_SELECT_MESSAGE_KEY);
+		if("create".equals(mode)){
+			Domain domain = Domain.getOrCreate(request.domain);
 			
-			sendBackWithValidationErrors();
+			User user = DemotivatorsSecurity.currentUser();	
+			Demotivator demo = new Demotivator(title, fileName, user, domain);
+			demo.save();
+			
+			success.put("id", demo.id.toString());
 		}
+		
+		success.put("status", "success");
+		success.put("fileName", "/image/" + fileName);
+		
+		renderJSON(success);
 	}
 
-	private static void sendBackWithValidationErrors() {
-		params.flash();			//Keeping values entered in form fields
-		Validation.keep();		//Keeping validation errors
-		add();					//Redirecting back to Add Demotivator page
+	@Util
+	private static void validateParameters(Map<String, String> errors, String title, String text, File[] image) {
+		if(StringUtils.isBlank(title)){
+			errors.put("title_error", Messages.get(REQUIRED));
+		} else if(title.length() > 30){
+			errors.put("title_error", Messages.get(TITLE_CANT_MESSAGE_KEY));
+		}
+		
+		if(text.length() > 80){
+			errors.put("text_error", Messages.get(TEXT_CANT_MESSAGE_KEY));
+		}
+		
+		if(image == null || image[0] == null){
+			errors.put("image_error", Messages.get(PLEASE_SELECT_MESSAGE_KEY));
+		}
 	}
+	
 }
--- test/controllers/CreatorTest.java
+++ test/controllers/CreatorTest.java
@@ -53,8 +53,7 @@ public class CreatorTest extends FunctionalTest{
     }
     
 	/**
-	 * Check Demotivator created and browser redirected to new single
-	 * Demotivator page. The title and text are max in size.
+	 * Check Demotivator created. The title and text are max in size.
 	 * @throws IOException
 	 */
     @Test
@@ -67,20 +66,48 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", THIRTY_CHARS_TITLE);
     	createDemoParams.put("text", EIGHTY_CHARS_TEXT);
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/image.jpg");
     	fileParams.put("image", file);
 
     	Response response = POST("/create", createDemoParams, fileParams);
 
-    	assertStatus(302, response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
     	
     	List<Demotivator> demos = Demotivator.findAll();
     	assertEquals(1, demos.size());
     	assertEquals(THIRTY_CHARS_TITLE, demos.get(0).getTitle());
     	long id = demos.get(0).getId().longValue();
     	
-    	assertHeaderEquals("Location", "/single/" + id, response);
+    	assertContentEquals("{\"fileName\":\"/image/test.file.name\",\"id\":\"" + id + "\",\"status\":\"success\"}", response);
+    }
+    
+    @Test
+    public void testDemotivatorPreviewSuccess() throws IOException{
+    	Fixtures.deleteAllModels();
+    	Fixtures.loadModels("data/user.yml");
+    	
+    	authenticate();
+    	
+    	Map<String, String> createDemoParams = new HashMap<String, String>();
+    	createDemoParams.put("title", THIRTY_CHARS_TITLE);
+    	createDemoParams.put("text", EIGHTY_CHARS_TEXT);
+    	createDemoParams.put("mode", "preview");
+    	Map<String, File> fileParams = new HashMap<String, File>();
+    	File file = new File("test/data/image.jpg");
+    	fileParams.put("image", file);
+
+    	Response response = POST("/create", createDemoParams, fileParams);
+
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	
+    	List<Demotivator> demos = Demotivator.findAll();
+    	assertEquals(0, demos.size());
+    	
+    	assertContentEquals("{\"fileName\":\"/image/test.file.name\",\"status\":\"success\"}", response);
     }
     
     @Test
@@ -93,14 +120,16 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", "");
     	createDemoParams.put("text", "ordinary text");
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/image.jpg");
     	fileParams.put("image", file);
 
     	Response response = POST("/create", createDemoParams, fileParams);
 
-    	assertStatus(302, response);
-    	assertHeaderEquals("Location", "/add", response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	assertContentEquals("{\"title_error\":\"Required\"}", response);
     }
     
     @Test
@@ -113,18 +142,20 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", THIRTY_CHARS_TITLE + "a");
     	createDemoParams.put("text", "ordinary text");
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/image.jpg");
     	fileParams.put("image", file);
 
     	Response response = POST("/create", createDemoParams, fileParams);
 
-    	assertStatus(302, response);
-    	assertHeaderEquals("Location", "/add", response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	assertContentEquals("{\"title_error\":\"Title can\\u0027t be longer than 30 characters.\"}", response);
     }
     
     @Test
-    public void testTextCantBeLongerThenHudnredTwentyWords(){
+    public void testTextCantBeLongerThenEightyWords(){
     	Fixtures.deleteAllModels();
     	Fixtures.loadModels("data/user.yml");
     	
@@ -133,14 +164,16 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", "ordinary title");
     	createDemoParams.put("text", EIGHTY_CHARS_TEXT + "a");
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/image.jpg");
     	fileParams.put("image", file);
 
     	Response response = POST("/create", createDemoParams, fileParams);
 
-    	assertStatus(302, response);
-    	assertHeaderEquals("Location", "/add", response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	assertContentEquals("{\"text_error\":\"Text can\\u0027t be longer than 80 characters.\"}", response);
     }
     
     @Test
@@ -153,11 +186,13 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", "ordinary title");
     	createDemoParams.put("text", "ordinary text");
+    	createDemoParams.put("mode", "create");
     	
     	Response response = POST("/create", createDemoParams);
 
-    	assertStatus(302, response);
-    	assertHeaderEquals("Location", "/add", response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	assertContentEquals("{\"image_error\":\"Please select an image.\"}", response);
     }
     
     @Test
@@ -170,14 +205,16 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", "ordinary title");
     	createDemoParams.put("text", "ordinary text");
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/bad.file");
     	fileParams.put("image", file);
     	
     	Response response = POST("/create", createDemoParams, fileParams);
 
-    	assertStatus(302, response);
-    	assertHeaderEquals("Location", "/add", response);
+    	assertStatus(200, response);
+    	assertContentType("application/json; charset=utf-8", response);
+    	assertContentEquals("{\"image_error\":\"Bad image file\"}", response);
     }
     
     @Test
@@ -188,15 +225,16 @@ public class CreatorTest extends FunctionalTest{
     	Map<String, String> createDemoParams = new HashMap<String, String>();
     	createDemoParams.put("title", THIRTY_CHARS_TITLE);
     	createDemoParams.put("text", EIGHTY_CHARS_TEXT);
+    	createDemoParams.put("mode", "create");
     	Map<String, File> fileParams = new HashMap<String, File>();
     	File file = new File("test/data/image.jpg");
     	fileParams.put("image", file);
 
     	Response response = POST("/create", createDemoParams, fileParams);
-
-    	 assertStatus(302, response);
-         
-         assertHeaderEquals("Location", "/secure/login", response);
+    	
+    	assertStatus(403, response);
+    	assertContentType("text/html; charset=utf-8", response);
+    	assertContentEquals("<h1>Please login</h1>", response);
     }    
     
     private void authenticate() {
