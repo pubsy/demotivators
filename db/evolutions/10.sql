65203df004bc34e91224ee6dbfaac77300342149
Vitalii Kravets <vitalii.kravets@gmail.com>
2014-06-25 23:27:16 +0200

    Fixing db evolution file

--- db/evolutions/10.sql
+++ db/evolutions/10.sql
@@ -1,11 +1,11 @@
 # --- !Ups
 
-ALTER TABLE User ADD COLUMN loginName VARCHAR(255) NOT NULL DEFAULT ''; 
+ALTER TABLE User ADD COLUMN loginName VARCHAR(255) NOT NULL; 
+
+UPDATE User SET loginName = email;
 
 ALTER TABLE User ADD CONSTRAINT unique_loginname 
 UNIQUE (loginName);
-
-UPDATE User SET loginName = email;
 
 # --- !Downs
 ALTER TABLE User DROP INDEX unique_loginname;
